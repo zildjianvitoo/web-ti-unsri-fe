163efec6fe74d6aa02d6f84748525d095618f156
zildjianvitoo <zildjianvito07@gmail.com>
2024-07-14 22:08:15 +0700

    feat(ProfilLulusan): kompetensi pendukung section

--- src/components/App/Profil/ProfilJurusan/KompetensiPendukung.tsx
+++ src/components/App/Profil/ProfilJurusan/KompetensiPendukung.tsx
@@ -3,6 +3,121 @@ export default function KompetensiPendukung() {
     <section
       id="kompetensi-utama"
       className="mx-auto mt-10 px-4 lg:mt-16 lg:px-12 xl:px-20"
-    ></section>
+    >
+      <div className="flex flex-col gap-6 lg:gap-7">
+        <div className="flex flex-col gap-6 lg:flex-row">
+          <div className="flex flex-1 flex-col gap-5">
+            <h2 className="from-secondary-100 to-primary-100 w-fit bg-gradient-to-r bg-clip-text text-3xl font-bold text-transparent max-lg:text-center sm:text-2xl md:text-3xl xl:text-4xl">
+              Kompetensi Pendukung.
+            </h2>
+            <h4 className="text-lg font-medium max-md:text-center sm:mt-5 lg:mt-3 lg:text-xl">
+              Beberapa kompetensi pendukung yang akan dipereoleh, diantaranya.
+            </h4>
+          </div>
+          <div className="relative flex flex-[2] flex-col rounded-2xl bg-primary/90 lg:rounded-[20px]">
+            <div className="flex flex-col px-8 py-4 lg:pr-14">
+              <div className="flex items-start gap-7">
+                <div className="mt-1 flex gap-2">
+                  <div className="size-6 rounded-full bg-[#71EDAA]" />
+                  <div className="size-6 rounded-full bg-[#E3A5A5]" />
+                  <div className="size-6 rounded-full bg-[#F0CCA2]" />
+                </div>
+                <h3 className="ml-auto mr-2 text-right text-2xl font-bold text-black lg:text-3xl">
+                  Manajemen <br />
+                  Proyek
+                </h3>
+              </div>
+              <p className="mt-5 text-justify text-black md:mt-8 lg:mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
+                metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
+              </p>
+            </div>
+            <div className="absolute right-0 rounded-bl-[10px] bg-background p-[2px] py-0 pl-3">
+              <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-8xl">
+                1
+              </p>
+            </div>
+          </div>
+          <div className="relative flex flex-[2] flex-col rounded-2xl bg-secondary/90 lg:rounded-[20px]">
+            <div className="flex flex-col px-8 py-4 lg:pr-14">
+              <div className="flex items-start gap-7">
+                <div className="mt-1 flex gap-2">
+                  <div className="size-6 rounded-full bg-[#71EDAA]" />
+                  <div className="size-6 rounded-full bg-[#E3A5A5]" />
+                  <div className="size-6 rounded-full bg-[#F0CCA2]" />
+                </div>
+                <h3 className="ml-auto text-right text-2xl font-bold text-black md:mr-4 lg:mr-6 lg:text-3xl">
+                  Bahasa <br />
+                  Inggris
+                </h3>
+              </div>
+              <p className="mt-5 text-justify text-black md:mt-8 lg:mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
+                metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
+              </p>
+            </div>
+            <div className="absolute right-0 rounded-bl-[10px] bg-background p-[2px] py-0 pl-3">
+              <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-8xl">
+                2
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6 md:flex-row">
+          <div className="relative flex flex-[2] flex-col rounded-2xl bg-primary/90 lg:rounded-[20px] lg:bg-secondary/90">
+            <div className="flex flex-col px-8 py-4 lg:pr-14">
+              <div className="flex items-start gap-5 lg:gap-7">
+                <div className="mt-1 flex gap-2">
+                  <div className="size-6 rounded-full bg-[#71EDAA]" />
+                  <div className="size-6 rounded-full bg-[#E3A5A5]" />
+                  <div className="size-6 rounded-full bg-[#F0CCA2]" />
+                </div>
+                <h3 className="ml-auto mr-2 text-right text-2xl font-bold text-black md:mr-6 lg:text-3xl">
+                  Komunikasi & <br className="hidden lg:block" />
+                  Kolaborasi
+                </h3>
+              </div>
+              <p className="mt-5 text-justify text-black md:mt-8 lg:mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
+                metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
+              </p>
+            </div>
+            <div className="absolute right-0 rounded-bl-[10px] bg-background p-[2px] py-0 pl-3">
+              <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-8xl">
+                3
+              </p>
+            </div>
+          </div>
+          <div className="relative flex flex-[2] flex-col rounded-2xl bg-secondary/90 lg:rounded-[20px] lg:bg-primary/90">
+            <div className="flex flex-col px-8 py-4 lg:pr-14">
+              <div className="flex items-start gap-2 lg:gap-7">
+                <div className="mt-1 flex gap-2">
+                  <div className="size-6 rounded-full bg-[#71EDAA]" />
+                  <div className="size-6 rounded-full bg-[#E3A5A5]" />
+                  <div className="size-6 rounded-full bg-[#F0CCA2]" />
+                </div>
+                <h3 className="ml-auto mr-2 text-right text-2xl font-bold text-black lg:mr-6 lg:text-3xl xl:mr-8">
+                  Etika Profesional & <br />
+                  Kepemimpinan
+                </h3>
+              </div>
+              <p className="mt-5 text-justify text-black md:mt-8 lg:mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
+                metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
+              </p>
+            </div>
+            <div className="absolute right-0 rounded-bl-[10px] bg-background p-[2px] py-0 pl-3">
+              <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-8xl">
+                4
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/components/App/Profil/ProfilJurusan/KompetensiUtama.tsx
+++ src/components/App/Profil/ProfilJurusan/KompetensiUtama.tsx
@@ -14,16 +14,16 @@ export default function KompetensiUtama() {
         </figure>
         <div className="flex flex-1 flex-col">
           <h2 className="from-secondary-100 to-primary-100 bg-gradient-to-r bg-clip-text text-3xl font-bold text-transparent max-lg:text-center sm:text-4xl md:text-5xl xl:text-6xl">
-            Kompetensi Utama
+            Kompetensi Utama.
           </h2>
-          <h4 className="mt-5 text-lg font-medium lg:mt-7 lg:text-xl">
+          <h4 className="mt-5 text-lg font-medium max-md:text-center lg:mt-7 lg:text-xl">
             Lulusan Program Studi Teknik Informatika pada Fakultas Ilmu Komputer
             Universitas Sriwijaya harus memiliki kompetensi, diantaranya:
           </h4>
           <div className="mt-5 flex flex-col gap-5">
-            <div className="relative rounded-r-2xl bg-primary lg:rounded-r-[20px]">
+            <div className="relative rounded-r-2xl bg-primary/90 lg:rounded-r-[20px]">
               <div className="absolute rounded-br-[10px] bg-background p-[2px] py-0 pr-2">
-                <p className="text-2xl/0 from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text font-bold text-transparent lg:text-3xl">
+                <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-3xl">
                   1
                 </p>
               </div>
@@ -32,7 +32,7 @@ export default function KompetensiUtama() {
                 yang luhur.
               </p>
             </div>
-            <div className="relative rounded-r-2xl bg-secondary lg:rounded-r-[20px]">
+            <div className="relative rounded-r-2xl bg-secondary/90 lg:rounded-r-[20px]">
               <div className="absolute rounded-br-[10px] bg-background p-[2px] py-0 pr-2">
                 <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-3xl">
                   2
@@ -43,7 +43,7 @@ export default function KompetensiUtama() {
                 serta berkemampuan dalam merumuskan masalah secara tepat
               </p>
             </div>
-            <div className="relative rounded-r-2xl bg-primary lg:rounded-r-[20px]">
+            <div className="relative rounded-r-2xl bg-primary/90 lg:rounded-r-[20px]">
               <div className="absolute rounded-br-[10px] bg-background p-[2px] py-0 pr-2">
                 <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-3xl">
                   3
@@ -54,7 +54,7 @@ export default function KompetensiUtama() {
                 perkembangan ilmu pengetahuan dan teknologi.
               </p>
             </div>
-            <div className="relative rounded-r-2xl bg-secondary lg:rounded-r-[20px]">
+            <div className="relative rounded-r-2xl bg-secondary/90 lg:rounded-r-[20px]">
               <div className="absolute rounded-br-[10px] bg-background p-[2px] py-0 pr-2">
                 <p className="from-secondary-100 to-primary-100 bg-gradient-to-br bg-clip-text text-2xl font-bold text-transparent lg:text-3xl">
                   4
