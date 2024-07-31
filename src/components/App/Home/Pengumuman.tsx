2d945e99fd58fa0a718f3e7bc704d3c7c8c5d08e
SiPilip <philifs.74hutar@gmail.com>
2024-07-31 22:56:15 +0700

    feat: pengumuman homepage, style: pengumuman homepage image

--- src/components/App/Home/Pengumuman.tsx
+++ src/components/App/Home/Pengumuman.tsx
@@ -1,4 +1,19 @@
+import ErrorScreen from "@/components/ErrorScreen";
+import LoadingScreen from "@/components/LoadingScreen";
+import formatDateTimeID from "@/lib/formatDateTimeID";
+import { getAllPengumuman } from "@/lib/network-data/pengumuman";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Pengumuman() {
+  const { data, isLoading, error } = useQuery({
+    queryFn: getAllPengumuman,
+    queryKey: ["pengumuman"],
+  });
+
+  if (isLoading) return <LoadingScreen />;
+
+  if (error) return <ErrorScreen />;
+
   return (
     <section
       className="mx-auto mt-10 px-4 lg:mt-16 lg:px-12 xl:px-20"
@@ -7,138 +22,27 @@ export default function Pengumuman() {
       <div className="flex flex-col gap-7 lg:gap-10">
         <h1 className="text-3xl font-bold md:text-4xl">Pengumuman</h1>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:gap-y-10">
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 lg:flex-row">
-            <figure className="md:basis-[25%]">
-              <img
-                src="/images/vector-informasi-jurusan.svg"
-                alt="Vector Informasi Jurusan "
-                className="object-contain"
-              />
-            </figure>
-            <div className="flex flex-col gap-4 md:basis-[75%]">
-              <h3 className="text-lg font-medium md:text-xl">
-                Teknik Informatika
-              </h3>
-              <p className="line-clamp-4 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. . Maecenas
-                aptent taciti... Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                tellus. . Maecenas aptent taciti
-              </p>
-              <p className="text-lg font-medium">07 Januari 2024</p>
-            </div>
-          </div>
+          {data?.map((item, index) => (
+            <a
+              className="flex flex-col items-center gap-4 lg:flex-row"
+              key={`berita-${index}`}
+            >
+              <figure className="h-full bg-gray-800 md:basis-[25%]">
+                <img
+                  src={item.thumbnail}
+                  alt="Vector Informasi Jurusan "
+                  className="h-full w-full object-cover"
+                />
+              </figure>
+              <div className="flex flex-col gap-4 md:basis-[75%]">
+                <h3 className="text-lg font-medium md:text-xl">{item.judul}</h3>
+                <p className="line-clamp-4 text-justify">{item.konten}</p>
+                <p className="text-lg font-medium">
+                  {formatDateTimeID(item.tanggalDibuat)}
+                </p>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </section>
