bfa28949928c831ce980dd955655a713dcffb03c
SiPilip <philifs.74hutar@gmail.com>
2024-08-01 02:24:54 +0700

    feat: berita homepage, style: berita homepage

--- src/components/App/Home/Berita.tsx
+++ src/components/App/Home/Berita.tsx
@@ -1,6 +1,20 @@
+import ErrorScreen from "@/components/ErrorScreen";
+import LoadingScreen from "@/components/LoadingScreen";
+import formatDateTimeID from "@/lib/formatDateTimeID";
+import { getAllBerita } from "@/lib/network-data/berita";
+import { useQuery } from "@tanstack/react-query";
 import { FaCalendar } from "react-icons/fa";
 
 export default function Berita() {
+  const { data, isLoading, error } = useQuery({
+    queryFn: getAllBerita,
+    queryKey: ["berita"],
+  });
+
+  if (isLoading) return <LoadingScreen />;
+
+  if (error) return <ErrorScreen />;
+
   return (
     <section
       className="mx-auto mt-10 px-4 lg:mt-16 lg:px-12 xl:px-20"
@@ -9,56 +23,30 @@ export default function Berita() {
       <div className="flex flex-col gap-7 lg:gap-10">
         <h1 className="text-center text-3xl font-bold md:text-4xl">Berita</h1>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-10">
-          <div className="flex flex-col gap-4">
-            <img
-              src="/images/carousel-image.png"
-              alt="Berita"
-              className="rounded-md"
-            />
-            <div className="flex flex-col gap-2">
-              <h3 className="text-xl font-semibold md:text-2xl">
-                Lorem ipsum dolor sit amet, adipiscing Etiam eu turpis.
-              </h3>
-              <div className="flex gap-1">
-                <FaCalendar className="size-5 text-[#5c5839] dark:text-white" />
-                <p className="text-sm">29 Jan 2024</p>
+          {data?.slice(0, 3).map((item, index) => (
+            <div className="flex flex-col gap-4" key={`berita-${index}`}>
+              <img
+                src={item.thumbnail}
+                alt="Berita"
+                className="max-h-72 w-full rounded-md object-cover"
+              />
+              <div className="flex flex-col gap-2">
+                <h3 className="text-xl font-semibold md:text-2xl">
+                  {item.judul}
+                </h3>
+                <div className="flex gap-1">
+                  <FaCalendar className="size-5 text-[#5c5839] dark:text-white" />
+                  <p className="text-sm">
+                    {formatDateTimeID(item.tanggalDibuat)}
+                  </p>
+                </div>
               </div>
+              <div
+                className="line-clamp-5 text-justify"
+                dangerouslySetInnerHTML={{ __html: item.konten || "" }}
+              ></div>
             </div>
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim. Class
-              aptent taciti sociosqu ad litora torquent per conubia nostra, per
-              inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-              tellus. Sed dignissim. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos hienaeos...
-            </p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <img
-              src="/images/carousel-image.png"
-              alt="Berita"
-              className="rounded-md"
-            />
-            <div className="flex flex-col gap-2">
-              <h3 className="text-xl font-semibold md:text-2xl">
-                Lorem ipsum dolor sit amet, adipiscing Etiam eu turpis.
-              </h3>
-              <div className="flex gap-1">
-                <FaCalendar className="size-5 text-[#5c5839] dark:text-white" />
-                <p className="text-sm">29 Jan 2024</p>
-              </div>
-            </div>
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-              turpis molestie, dictum est a, mattis tellus. Sed dignissim. Class
-              aptent taciti sociosqu ad litora torquent per conubia nostra, per
-              inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-              tellus. Sed dignissim. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos hienaeos...
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </section>
