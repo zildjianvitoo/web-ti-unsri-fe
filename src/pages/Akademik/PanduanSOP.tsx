bb04b2574fc287f425c098768df9122262013765
SiPilip <philifs.74hutar@gmail.com>
2024-08-03 00:12:22 +0700

    feat: panduan & sop page

---
+++ src/pages/Akademik/PanduanSOP.tsx
@@ -0,0 +1,80 @@
+import SectionTitle from "@/components/SectionTitle";
+import { cn } from "@/lib/utils";
+
+const data = [
+  {
+    title: "Panduan",
+    konten: "Lorem",
+    data: [
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+  {
+    title: "SOP",
+    konten: "Lorem",
+    data: [
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+      {
+        nama: "Panduan Kerja Praktik",
+        link: "https://www.google.com",
+      },
+    ],
+  },
+];
+
+export default function PanduanSOP() {
+  return (
+    <>
+      <section id="informasi-jurusan">
+        <div className="flex flex-col gap-8 lg:gap-10">
+          <SectionTitle title="Panduan & SOP" />
+        </div>
+        <div className="mt-10 flex h-full w-full flex-col gap-10 px-4 md:gap-14 lg:px-12 xl:px-20">
+          {data?.map((item, index) => (
+            <div className="flex flex-col gap-2" key={index + "data"}>
+              <h1 className="w-fit bg-gradient-to-r from-secondary-100 to-primary-100 bg-clip-text text-2xl font-bold text-transparent lg:text-5xl">
+                {item.title}
+              </h1>
+              <p className="text-sm lg:text-lg">{item.konten}</p>
+              <div className="lg:gap- grid grid-flow-row grid-cols-1 gap-2 lg:grid-cols-2">
+                {item.data.map((item, index) => (
+                  <a
+                    className={cn(
+                      `rounded-md border-2 border-primary px-2 py-2 font-semibold max-md:text-sm`,
+                      {
+                        "even:border-secondary md:border-secondary":
+                          index % 4 < 2,
+                      },
+                    )}
+                    href={item.link}
+                    key={index + "button"}
+                  >
+                    {item.nama}
+                  </a>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
