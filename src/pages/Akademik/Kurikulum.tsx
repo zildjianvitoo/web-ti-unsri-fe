74bdc2ac7d75716663f9638630761d3d6e6680a6
SiPilip <philifs.74hutar@gmail.com>
2024-08-03 00:12:10 +0700

    style: kurikulum

--- src/pages/Akademik/Kurikulum.tsx
+++ src/pages/Akademik/Kurikulum.tsx
@@ -7,10 +7,10 @@ export default function Kurikulum() {
         <div className="flex flex-col gap-8 lg:gap-10">
           <SectionTitle title="Kurikulum" />
         </div>
-        {Array.from({ length: 2 }).map((item, index) => (
-          <div className="mt-10 flex h-full w-full flex-col gap-10 px-4 md:gap-14 lg:px-12 xl:px-20">
-            <div className="flex flex-col gap-2">
-              <h1 className="bg-gradient-to-r from-secondary-100 to-primary-100 bg-clip-text text-2xl font-bold text-transparent lg:text-5xl">
+        <div className="mt-10 flex h-full w-full flex-col gap-10 px-4 md:gap-14 lg:px-12 xl:px-20">
+          {Array.from({ length: 2 }).map((item, index) => (
+            <div className="flex flex-col gap-2" key={index + "kurikulum"}>
+              <h1 className="w-fit bg-gradient-to-r from-secondary-100 to-primary-100 bg-clip-text text-2xl font-bold text-transparent lg:text-5xl">
                 Kurikulum 2019
               </h1>
               <p className="text-sm lg:text-lg">
@@ -30,8 +30,8 @@ export default function Kurikulum() {
                 </button>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
     </>
   );
