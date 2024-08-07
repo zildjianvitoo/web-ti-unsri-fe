772b2b76799e5dbd2e3532a9b6001ef851d45dc3
SiPilip <philifs.74hutar@gmail.com>
2024-08-07 20:38:06 +0700

    style: visi misi

--- src/components/App/Profil/InformasiJurusan/VisiMisi.tsx
+++ src/components/App/Profil/InformasiJurusan/VisiMisi.tsx
@@ -2,7 +2,6 @@ import ErrorScreen from "@/components/ErrorScreen";
 import LoadingScreen from "@/components/LoadingScreen";
 import { getAllInformasiJurusan } from "@/lib/network-data/informasiJurusan";
 import { useQuery } from "@tanstack/react-query";
-import { BsPeopleFill } from "react-icons/bs";
 
 export default function VisiMisi() {
   const { data, isLoading, error } = useQuery({
@@ -28,19 +27,34 @@ export default function VisiMisi() {
             </p>
           </div>
         </div>
-        <h3 className="-mb-7 text-center text-2xl lg:mb-0 lg:hidden lg:text-start lg:text-4xl">
+        <h3 className="-mb-3 text-center text-2xl lg:mb-0 lg:hidden lg:text-start lg:text-4xl">
           Misi
         </h3>
         <div className="gap-8 md:flex md:flex-row lg:w-7/12">
           <div className="w-full md:w-6/12 lg:w-6/12">
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-5">
               <h3 className="hidden md:text-xl lg:block lg:text-4xl">Misi</h3>
               {data?.misi.slice(0, 2).map((item, index) => (
                 <div
                   className="flex flex-col items-center gap-1 text-center lg:text-start"
                   key={index}
                 >
-                  <BsPeopleFill className="text-7xl text-green-300" />
+                  <img
+                    src={item.icon}
+                    alt=""
+                    className="w-10"
+                    style={
+                      index !== 1
+                        ? {
+                            filter:
+                              "brightness(0) saturate(100%) invert(84%) sepia(26%) saturate(735%) hue-rotate(86deg) brightness(98%) contrast(90%)",
+                          }
+                        : {
+                            filter:
+                              "brightness(0) saturate(100%) invert(94%) sepia(38%) saturate(3463%) hue-rotate(300deg) brightness(111%) contrast(96%)",
+                          }
+                    }
+                  />
                   <h4 className="text-xl font-bold lg:text-2xl">
                     {item.judul}
                   </h4>
@@ -51,14 +65,29 @@ export default function VisiMisi() {
               ))}
             </div>
           </div>
-          <div className="w-full md:w-6/12 lg:w-6/12 lg:pt-28">
+          <div className="mt-5 w-full md:w-6/12 lg:w-6/12 lg:pt-28">
             <div className="flex flex-col gap-4">
               {data?.misi.slice(2, 4).map((item, index) => (
                 <div
                   className="flex flex-col items-center gap-1 text-center lg:text-start"
                   key={index}
                 >
-                  <BsPeopleFill className="text-7xl text-green-300" />
+                  <img
+                    src={item.icon}
+                    alt=""
+                    className="w-14"
+                    style={
+                      index === 1
+                        ? {
+                            filter:
+                              "brightness(0) saturate(100%) invert(79%) sepia(42%) saturate(2667%) hue-rotate(206deg) brightness(104%) contrast(89%)",
+                          }
+                        : {
+                            filter:
+                              "brightness(0) saturate(100%) invert(96%) sepia(100%) saturate(6906%) hue-rotate(290deg) brightness(95%) contrast(87%)",
+                          }
+                    }
+                  />
                   <h4 className="text-center text-xl font-bold lg:text-start lg:text-2xl">
                     {item.judul}
                   </h4>
