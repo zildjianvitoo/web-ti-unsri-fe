c4f5a89839324e85dc04c8ce308c16367554193e
SiPilip <philifs.74hutar@gmail.com>
2024-08-07 22:26:08 +0700

    style: informasi jurusan

--- src/components/App/Home/InformasiJurusan.tsx
+++ src/components/App/Home/InformasiJurusan.tsx
@@ -8,7 +8,7 @@ export default function InformasiJurusan() {
       id="informasi-jurusan"
     >
       <div className="flex flex-col gap-6 lg:flex-row lg:gap-8">
-        <div className="flex min-h-[20rem] flex-col justify-center gap-4 rounded-md bg-gradient-to-br from-primary/70 to-secondary/70 p-6 sm:p-10 md:p-12 lg:rounded-xl lg:p-[60px]">
+        <div className="flex min-h-[20rem] basis-7/12 flex-col justify-center gap-4 rounded-md bg-gradient-to-br from-primary/70 to-secondary/70 p-6 sm:p-10 md:p-12 lg:rounded-xl lg:p-[60px]">
           <h1 className="text-3xl font-bold lg:text-[40px]">
             Informasi Jurusan
           </h1>
@@ -22,7 +22,7 @@ export default function InformasiJurusan() {
             <FiArrowUpRight className="size-5" />
           </Link>
         </div>
-        <div className="relative flex min-h-[20rem] flex-col justify-center gap-4 overflow-hidden rounded-md bg-gradient-to-br from-primary/70 to-secondary/70 lg:rounded-xl">
+        <div className="relative flex min-h-[20rem] basis-5/12 flex-col justify-center gap-4 overflow-hidden rounded-md bg-gradient-to-br from-primary/70 to-secondary/70 lg:rounded-xl">
           <div className="absolute h-full w-full bg-gradient-to-br from-primary/70 to-secondary/70 opacity-60" />
           <img
             src="../../../../public/images/fasilkom.jpg"
