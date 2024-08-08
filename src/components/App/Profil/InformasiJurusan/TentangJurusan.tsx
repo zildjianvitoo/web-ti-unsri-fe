900863cf53dcc4b19813e181ac233a26ae48c89a
SiPilip <philifs.74hutar@gmail.com>
2024-08-08 10:55:07 +0700

    style: floating tentang jrusan

--- src/components/App/Profil/InformasiJurusan/TentangJurusan.tsx
+++ src/components/App/Profil/InformasiJurusan/TentangJurusan.tsx
@@ -17,24 +17,24 @@ export default function TentangJurusan() {
   return (
     <section
       id="tentang-jurusan"
-      className="mt-20 flex w-full flex-col gap-4 px-4 md:gap-8 lg:flex-row lg:px-12 xl:px-20"
+      className="mt-20 w-full gap-4 px-4 max-md:flex max-md:flex-col md:gap-8 lg:px-12 xl:px-20"
     >
-      <div className="max-h-[50vh] overflow-hidden rounded-2xl lg:w-3/6 xl:w-2/6">
+      <div className="float-left mr-5 overflow-hidden rounded-2xl md:h-96 lg:w-3/6 xl:w-2/6">
         <img
           src={img}
           alt="img-informasijurusan"
           className="h-full w-full object-cover"
         />
       </div>
-      <div className="flex flex-col text-justify text-base leading-5 md:text-lg md:leading-6 lg:w-3/6 lg:gap-2 xl:w-4/6">
+      <div className="text-justify text-base leading-5 md:text-lg md:leading-6 lg:gap-2">
         <h1 className="text-center text-2xl font-bold md:text-start md:text-4xl">
           Teknik Informatika
         </h1>
-        <h3 className="mb-2 text-center text-lg font-semibold md:text-start md:text-xl lg:mb-0">
+        <h3 className="mb-2 text-center text-lg font-semibold md:text-start md:text-2xl lg:mb-0">
           Universitas Sriwijaya
         </h3>
         <div
-          className="text-justify"
+          className="block text-justify text-lg"
           dangerouslySetInnerHTML={{ __html: data?.informasi || "" }}
         ></div>
       </div>
